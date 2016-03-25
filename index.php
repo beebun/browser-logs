b5708aa0b090ad8be6a954a95276590f663da390
Bun Suwanparsert <bun.swp@gmail.com>
2016-03-25 17:53:20 +0700

    Show overview info

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php	
 	require 'vendor/autoload.php';
 	$result = new WhichBrowser\Parser($_SERVER['HTTP_USER_AGENT']);
-	echo "<pre>";
-	print_r($result);
-	echo "</pre>";
+	// echo "<pre>";
+	// print_r($result);
+	// echo "</pre>";
 ?>
 
 You are using: <?php echo $result->toString(); ?><br/>
